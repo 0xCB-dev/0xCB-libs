9a985515f6c0433a841f9bf5d098517703f6810d
Conor Burns <mail@conor-burns.com>
2022-04-19 00:14:18 +0200

    lets try that again

--- PCM/create_pcm_archive.sh
+++ PCM/create_pcm_archive.sh
@@ -10,20 +10,15 @@ rm -rf PCM/archive
 
 
 echo "Create folder structure for ZIP"
-mkdir -p PCM/archive/plugins
 mkdir -p PCM/archive/resources
 
 echo "Copy files to destination"
-cp VERSION PCM/archive/plugins
-cp *.py PCM/archive/plugins
-cp *.png PCM/archive/plugins
-cp settings.json PCM/archive/plugins
-cp -r icons PCM/archive/plugins
+cp -r LIB/* PCM/archive/
 cp PCM/icon.png PCM/archive/resources
 cp PCM/metadata.template.json PCM/archive/metadata.json
 
 echo "Write version info to file"
-echo $VERSION > PCM/archive/plugins/VERSION
+echo $VERSION > PCM/archive/VERSION
 
 echo "Modify archive metadata.json"
 sed -i "s/VERSION_HERE/$VERSION/g" PCM/archive/metadata.json
@@ -42,6 +37,6 @@ echo "Gather data for repo rebuild"
 echo VERSION=$VERSION >> $GITHUB_ENV
 echo DOWNLOAD_SHA256=$(shasum --algorithm 256 PCM/KiCAD-PCM-$VERSION.zip | xargs | cut -d' ' -f1) >> $GITHUB_ENV
 echo DOWNLOAD_SIZE=$(ls -l PCM/KiCAD-PCM-$VERSION.zip | xargs | cut -d' ' -f5) >> $GITHUB_ENV
-echo DOWNLOAD_URL="https:\/\/github.com\/Bouni\/kicad-jlcpcb-tools\/releases\/download\/$VERSION\/KiCAD-PCM-$VERSION.zip" >> $GITHUB_ENV
+echo DOWNLOAD_URL="https:\/\/github.com\/0xCB-dev\/0xCB-libs\/releases\/download\/$VERSION\/KiCAD-PCM-$VERSION.zip" >> $GITHUB_ENV
 echo INSTALL_SIZE=$(unzip -l PCM/KiCAD-PCM-$VERSION.zip | tail -1 | xargs | cut -d' ' -f1) >> $GITHUB_ENV
 
